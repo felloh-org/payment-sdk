a721677bc6177a033fc8fb689ef0dfa68b346343
Adam <adam@felloh.org>
2022-09-27 11:14:27 +0100

    fix: update readme and add comments to SDK

---
+++ README.md
@@ -0,0 +1,6 @@
+# Felloh Javascript SDK
+
+[![Version](https://img.shields.io/npm/v/@felloh-org/payment-sdk.svg)](https://www.npmjs.com/package/@felloh-org/payment-sdk)
+[![Build Status](https://github.com/felloh-org/payment-sdk/actions/workflows/release.yml/badge.svg?branch=main)](https://github.com/felloh-org/payment-sdk/actions/workflows/release.yml)
+[![Downloads](https://img.shields.io/npm/dm/@felloh-org/payment-sdk.svg)](https://www.npmjs.com/package/@felloh-org/payment-sdk)
+[![Try on RunKit](https://badge.runkitcdn.com/@felloh-org/payment-sdk.svg)](https://runkit.com/npm/@felloh-org/payment-sdk)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
+/**
+ * PaymentSDK Class
+ */
 class PaymentsSDK {
 
-  constructor(containerName, sandbox = false) {
+  /**
+   * Payments SDK Constructor
+   * @param containerName
+   * @param publicKey
+   * @param options?
+   */
+  constructor(
+    containerName = null,
+    publicKey = null,
+    options = {
+      sandbox: false
+    },
+  ) {
+    // Set the public key
+    if (publicKey === null) {
+      throw new Error('Felloh Payment SDK: Public key must be defined')
+    }
+
+    this.publicKey = publicKey;
+
+    // Find the element and define it
+    if (containerName === null) {
+      throw new Error('Felloh Payment SDK: Container name must be defined')
+    }
+
     this.targetElement = document.getElementById(containerName);
 
     if(this.targetElement === null) {
       throw new Error('Felloh Payment SDK: Target element does not exist')
     }
 
-    this.baseURL = sandbox === true ? 'https://pay.sandbox.felloh.com/embed/' : 'https://pay.felloh.com/embed/';
+    // Set the base url for the pay frontend
+    this.baseURL = options.sandbox === true ? 'https://pay.sandbox.felloh.com/embed/' : 'https://pay.felloh.com/embed/';
+
+    // Define a unique ID for the Iframe
     this.iframeID = `iframe-${new Date().getTime()}`;
   }
 
+  /**
+   * Add listeners to the IFrame
+   * @returns {PaymentsSDK}
+   */
   addIFrameSizeListener() {
     const eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
     const eventer = window[eventMethod];
@@ -27,25 +61,41 @@ class PaymentsSDK {
         }
       } catch (e) {}
     }, false);
+
+    return this;
   }
 
+  /**
+   * Render the IFrame
+   * @param paymentID
+   * @returns {PaymentsSDK}
+   */
   renderIframe(paymentID) {
     const iframe = document.createElement("iframe");
-    iframe.setAttribute("src", `https://pay.felloh.com/embed/${paymentID}`);
+    iframe.setAttribute("src", `${this.baseURL}embed/${paymentID}`);
     iframe.style.width = "100%";
-    iframe.style.height = "100px";
+    iframe.style.height = "900px";
     iframe.id = this.iframeID;
 
     this.targetElement.innerHTML = '';
     this.targetElement.appendChild(iframe);
 
     this.addIFrameSizeListener();
+
+    return this;
   }
 
+  /**
+   * Render the payment session
+   * @param paymentID
+   * @returns {PaymentsSDK}
+   */
   render(paymentID) {
     if (this.targetElement !== null) {
       this.renderIframe(paymentID);
     }
+
+    return this;
   }
 }
 
